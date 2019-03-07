8e4b0441f95c60db722281ffd1fb23c847e21b85
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-03-06 19:38:08 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+$('#flashMessage').hide();
+
+$('#previewButton').click(function(){
+    const title = $('#blogTitleInput').val();
+    const content = $('#blogContentInput').val();
+  
+    $('#blogTitlePreview').text(title);
+    $('#blogContentPreview').text(content);
+  
+    $('#flashMessage')
+    .fadeIn(1000)
+    .delay(2500)
+    .slideUp();
+});
